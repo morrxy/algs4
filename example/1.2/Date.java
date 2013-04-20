e8d4a3675d9fdc3b2599829c9f1e2c2388c2c59f
morrxy <morrxy@gmail.com>
2013-04-21 00:04:11 +0800

    add 3 example

---
+++ example/1.2/Date.java
@@ -0,0 +1,39 @@
+/*
+ * java Date 12 21 1999 
+ */
+
+public class Date{
+	private final int month;
+	private final int day;
+	private final int year;
+
+	public Date(int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+	}
+
+	public int month() {
+		return month;
+	}
+
+	public int day() {
+		return day;
+	}
+
+	public int year() {
+		return year;
+	}
+
+	public String toString() {
+		return month() + "/" + day() + "/" + year();
+	}
+
+	public static void main(String[] args) {
+		int m = Integer.parseInt(args[0]);
+		int d = Integer.parseInt(args[1]);
+		int y = Integer.parseInt(args[2]);
+		Date date = new Date(m, d, y);
+		StdOut.println(date);
+	}
+}
---
+++ example/1.2/Date2.java
@@ -0,0 +1,36 @@
+/*
+ * java Date2 12 21 1999 
+ */
+
+public class Date2{
+
+ private final int value;
+
+ public Date2(int m, int d, int y) {
+  value = y * 512 + m * 32 + d;
+ }
+
+ public int month() {
+  return (value / 32) % 16;
+ }
+
+ public int day() {
+  return value % 32;
+ }
+
+ public int year() {
+  return value / 512;
+ }
+
+ public String toString() {
+  return month() + "/" + day() + "/" + year();
+ }
+
+ public static void main(String[] args) {
+  int m = Integer.parseInt(args[0]);
+  int d = Integer.parseInt(args[1]);
+  int y = Integer.parseInt(args[2]);
+  Date2 date = new Date2(m, d, y);
+  StdOut.println(date);
+ }
+}
