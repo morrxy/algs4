3cc23f5fa4bafe17065de4f9739de96e4c0a9432
morrxy <morrxy@gmail.com>
2013-04-26 23:14:47 +0800

    add 1 exercise

--- exercise/1.1/ex_1_1_31_2.java
+++ exercise/1.1/ex_1_1_31_2.java
@@ -4,10 +4,10 @@
  * .05 on the circumference of a circle, and then, with probability p for each pair of points, 
  * draws a gray line connecting them.
  *
- * java ex_1_1_31.java 10 .3
+ * java ex_1_1_31_2.java 10 .3
  */
 
-public class ex_1_1_31 {
+public class ex_1_1_31_2 {
 
 	public static final double CENTER_X = .5;
 	public static final double CENTER_Y = .5;
@@ -15,19 +15,25 @@ public class ex_1_1_31 {
 
 
 	public static void main(String[] args) {
-		StdDraw.circle(CENTER_X, CENTER_Y, RADIUS);
+		// StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+		// StdDraw.circle(CENTER_X, CENTER_Y, RADIUS);
 
 		int N = Integer.parseInt(args[0]);
+		if (N <= 0) return;
+
 		double probability = Double.parseDouble(args[1]);
 		Point2D[] points = new Point2D[N];
 
 		StdDraw.setPenColor(StdDraw.RED);
 		StdDraw.setPenRadius(.05);
 
+		Point2D center = new Point2D(CENTER_X, CENTER_Y);
+		Point2D pt1;
+		double average_angel = 360 / N;
+
 		for (int i = 0; i < N; i++) {
-			double x1 = Math.random();
-			double y1 = Math.random();
-			points[i] = new Point2D(x1, y1);
+			pt1 = get_point(center, RADIUS, average_angel * i);
+			points[i] = new Point2D(pt1.x(), pt1.y());
 			points[i].draw();
 		}
 
@@ -35,19 +41,27 @@ public class ex_1_1_31 {
 		StdDraw.setPenRadius();
 
 		for (int j = 0; j < points.length; j++) {
-			// k is next point in array to draw a line with j
 			int k = j + 1;
-			// draw every line using point j and point after j in array
-			while(true) {
-				if (k > points.length - 1) {
-					break;
-				}
-				if (StdRandom.bernoulli(probability)) {
-					StdDraw.line(points[j].x(), points[j].y(), points[k].x(), points[k].y());
-				}
-				k += 1;
+			if (k == points.length) {
+				k = 0;
+			}
+			if (StdRandom.bernoulli(probability)) {
+				StdDraw.line(points[j].x(), points[j].y(), points[k].x(), points[k].y());
 			}
 		}
+
+	}
+
+	public static Point2D get_point(Point2D center, double radius, double angel) {
+
+		double arc = (angel * Math.PI) / 180;
+
+		double dx = RADIUS * Math.cos(arc);
+		double dy = RADIUS * Math.sin(arc);
+
+		Point2D p2d = new Point2D(center.x() + dx, center.y() + dy);
+
+		return p2d;
 
 	}
 
