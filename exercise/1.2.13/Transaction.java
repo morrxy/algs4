1857a56bb16debe34756e94da6c9dcc31c4fbd3d
morrxy <morrxy@gmail.com>
2013-05-12 14:27:42 +0800

    add a exercise

--- exercise/1.2.13/Transaction.java
+++ exercise/1.2.13/Transaction.java
@@ -1,16 +1,43 @@
 /** 
- * 1.2.13 UsingourimplementationofDateasamodel(page91),
+ * 1.2.13 Usingourimplementation of Dateasamodel(page91),
  * develop an implementation of Transaction.
- * public class Transaction implements Comparable<Transaction>
- *              Transaction(String who, Date when, double amount)
- * 锟糡ransaction(String transaction) String who()
- * Date when() double amount() String toString()
- * boolean equals(Object that)
- * int compareTo(Transaction that) int hashCode()
- * create a transaction (parse constructor) customer name
- * date
- * amount
- * string representation
- * is this the same transaction as that?
- * compare this transaction to that hash code
+ *
+ * java Transaction
+ * Tom 
+ * 5/11/2013 
+ * 3000.0
+ * 
  */
+
+public class Transaction {
+
+	private String who;
+	private Date when;
+	private double amount;
+
+	Transaction(String who1, Date when1, double amount1) {
+		who = who1;
+		when = when1;
+		amount = amount1;
+	}
+
+	public String who() {
+		return who;
+	}
+
+	public Date when() {
+		return when;
+	}
+
+	public double amount() {
+		return amount;
+	}
+
+	public static void main(String[] args) {
+		Transaction t = new Transaction("Tom", new Date(5, 11, 2013), 3000);
+		StdOut.println(t.who());
+		StdOut.println(t.when());
+		StdOut.println(t.amount());
+	}
+
+}
