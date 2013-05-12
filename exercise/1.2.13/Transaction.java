013000e469204cc4ecea0099e448dedd46355d96
morrxy <morrxy@gmail.com>
2013-05-12 13:51:23 +0800

    a

---
+++ exercise/1.2.13/Transaction.java
@@ -0,0 +1,16 @@
+/** 
+ * 1.2.13 UsingourimplementationofDateasamodel(page91),
+ * develop an implementation of Transaction.
+ * public class Transaction implements Comparable<Transaction>
+ *              Transaction(String who, Date when, double amount)
+ * 锟糡ransaction(String transaction) String who()
+ * Date when() double amount() String toString()
+ * boolean equals(Object that)
+ * int compareTo(Transaction that) int hashCode()
+ * create a transaction (parse constructor) customer name
+ * date
+ * amount
+ * string representation
+ * is this the same transaction as that?
+ * compare this transaction to that hash code
+ */
