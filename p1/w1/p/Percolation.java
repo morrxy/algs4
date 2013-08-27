4773d19dd896114324ee028715d4f768da35f933
morrxy <morrxy@gmail.com>
2013-08-27 23:30:14 +0800

    a

--- p1/w1/p/Percolation.java
+++ p1/w1/p/Percolation.java
@@ -1,33 +1,34 @@
 public class Percolation {
-  public WeightedQuickUnionUF uf;
-  // private int totalSites;
-  private int size;
+  private WeightedQuickUnionUF uf;
   private boolean[] sitesStatus;
+  private int size;
   private int virtual_top;
   private int virtual_bottom;
 
   /** create N-by-N grid, with all sites blocked */
   public Percolation(int N) {
-    // totalSites = N * N;
     size = N;
-    uf = new WeightedQuickUnionUF(N * N + 2);
-    virtual_top = N * N;
-    virtual_bottom = N * N + 1;
+    // the last two site for virtual_top and virtual_bottom
+    uf = new WeightedQuickUnionUF(size * size + 2);
+    virtual_top = size * size;
+    virtual_bottom = size * size + 1;
 
-    sitesStatus = new boolean[N*N + 2];
-    for (int i = 0; i < N*N; i++) {
+    // initialize all site blocked
+    sitesStatus = new boolean[size*size + 2];
+    for (int i = 0; i < size*size; i++) {
       sitesStatus[i] = false;
     }
+    // initialize two virtual site open
     sitesStatus[virtual_top] = true;
     sitesStatus[virtual_bottom] = true;
 
     // connect each site in first row to virtual_top
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < size; i++) {
       uf.union(virtual_top, i);
     }
 
     // connect each site in last row to virtual_bottom
-    for (int i = (N-1)*N; i < N*N; i++) {
+    for (int i = (size-1)*size; i < size*size; i++) {
       uf.union(virtual_bottom, i);
     }
 
@@ -40,14 +41,14 @@ public class Percolation {
       throw new IndexOutOfBoundsException("row or/and column index out of bounds");
     }
 
-    int index_in_uf = xyTo1D(i, j);
-
-    sitesStatus[index_in_uf] = true;
-
-    connect_two_site(i, j, i-1, j);
-    connect_two_site(i, j, i+1, j);
-    connect_two_site(i, j, i  , j-1);
-    connect_two_site(i, j, i  , j+1);
+    if (!isOpen(i, j)) {
+      int index_in_uf = xyTo1D(i, j);
+      sitesStatus[index_in_uf] = true;
+      connect_two_site(i, j, i-1, j);
+      connect_two_site(i, j, i+1, j);
+      connect_two_site(i, j, i  , j-1);
+      connect_two_site(i, j, i  , j+1);
+    }
 
   }
 
@@ -56,7 +57,6 @@ public class Percolation {
       if (isOpen(x2, y2)) {
         int p = xyTo1D(x1, y1);
         int q = xyTo1D(x2, y2);
-
         uf.union(p, q);
       }
     }
--- p1/w1/p/PercolationStats.java
+++ p1/w1/p/PercolationStats.java
@@ -1,13 +1,50 @@
 public class PercolationStats {
+  private double[] results;
+  private int size;
+  private int experiment_times;
 
   /** perform T independent computational experiments on N-by_N grid */
   public PercolationStats(int N, int T) {
+    experiment_times = T;
+    size = N;
+    results = new double[experiment_times];
 
+    for (int i = 0; i < experiment_times; i++) {
+      results[i] = percolation_once(size);
+      // StdOut.println(results[i]);
+    }
+  }
+
+  private double percolation_once(int n) {
+    Percolation p = new Percolation(n);
+    int opened = 0;
+    while(true) {
+      if (p.percolates()) break;
+
+      // select a non-open site
+      int row;
+      int column;
+      while(true) {
+        row = StdRandom.uniform(1, size + 1);
+        column = StdRandom.uniform(1, size + 1);
+        if (!p.isOpen(row, column)) break;
+      }
+
+      // open that non-open site
+      p.open(row, column);
+      opened += 1;
+    }
+
+    return opened / (size * size * 1.0);
   }
 
   /** sample mean of percolation threshold */
   public double mean() {
-    return 0.0;
+    double t = 0;
+    for (int i = 0; i < experiment_times; i++) {
+      t += results[i];
+    }
+    return t / experiment_times;
   }
 
   /** sample standard deviation of percolation threshold */
@@ -30,8 +67,10 @@ public class PercolationStats {
     int N = Integer.parseInt(args[0]);
     int T = Integer.parseInt(args[1]);
 
-    StdOut.println("N:" + N);
-    StdOut.println("T:" + T);
+    PercolationStats ps = new PercolationStats(N, T);
+    StdOut.printf("%-23s = %f\n", "mean", ps.mean());
+    StdOut.printf("%-23s = %f\n", "stddev", ps.stddev());
+    StdOut.printf("%-23s = %f, %f\n", "95% confidence interval", ps.confidenceLo(), ps.confidenceHi());
   }
 
 }
