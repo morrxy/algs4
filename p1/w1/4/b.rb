e006ef11311dc7da5802ee5343dec0f191ed9f4c
morrxy <morrxy@gmail.com>
2013-08-29 23:25:08 +0800

    a

--- p1/w1/4/b.rb
+++ p1/w1/4/b.rb
@@ -1,8 +1,8 @@
-n1 = 6561
-t1 = 360.02
+n1 = 400
+t1 = 1.739
 
-n2 = 19683
-t2 = 10349.47
+n2 = 800
+t2 = 12.572
 
 td = Math.log2(t2) - Math.log2(t1)
 nd = Math.log2(n2) - Math.log2(n1)
---
+++ p1/w1/j/analysis/q1/DoublingRatio2.java
@@ -0,0 +1,23 @@
+public class DoublingRatio2 {
+
+  public static double timeTrial(int N) {
+    int MAX = 1000000;
+    int[] a = new int[N];
+    for (int i = 0; i < N; i++) {
+      a[i] = StdRandom.uniform(-MAX, MAX);
+    }
+    Stopwatch timer = new Stopwatch();
+    // int cnt = ThreeSum.count(a);
+    int cnt = ThreeSumFast.count(a);
+    return timer.elapsedTime();
+  }
+
+  public static void main(String[] args) {
+    double prev = timeTrial(125);
+    for (int N = 250; true; N += N) {
+      double time = timeTrial(N);
+      StdOut.printf("%6d %7.1f %5.1f\n", N, time, time/prev);
+    }
+  }
+
+}
