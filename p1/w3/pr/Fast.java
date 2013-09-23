c986f3d638d6b830158a03675dd54186e7093650
morrxy <morrxy@gmail.com>
2013-09-24 00:23:11 +0800

    a

--- p1/w3/pr/Fast.java
+++ p1/w3/pr/Fast.java
@@ -5,6 +5,85 @@ public class Fast {
     Arrays.sort(ps, p.SLOPE_ORDER);
   }
 
+  // is ps sorted of slope to p?
+  public static boolean isSorted(Point p, Point[] ps) {
+    for (int i = 1; i < ps.length; i++) {
+      if (p.slopeTo(ps[i]) < p.slopeTo(ps[i-1])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static Point[] rightPoints(int i, Point[] ps) {
+    Point[] arr = new Point[ps.length - i - 1];
+    for (int k = 0; k < arr.length; k++) {
+      arr[k] = ps[k + i + 1];
+    }
+    return arr;
+  }
+
+  public static void showPoints(Point[] ps) {
+    StdOut.println();
+    for (Point p : ps) {
+      StdOut.print(p.toString() + ", ");
+    }
+    StdOut.println();
+  }
+
+  public static void showResultPoints(Point[] ps) {
+    StdOut.println();
+    for (int i = 0; i < ps.length; i++) {
+      StdOut.print(ps[i].toString());
+      if (i != ps.length - 1) StdOut.print(" -> ");
+    }
+    StdOut.println();
+  }
+
+  public static void outPut(Point[] ps, int from, int to, Point p0) {
+
+    StdOut.println("output");
+    StdOut.println(p0.toString());
+    showPoints(ps);
+    StdOut.println("from:" + from);
+    StdOut.println("to:" + to);
+
+
+    Point[] arr = new Point[to - from + 1 + 1];
+    for (int i = 0; i < arr.length - 1 ; i++) {
+      arr[i] = ps[from];
+      from += 1;
+    }
+    arr[arr.length - 1] = p0;
+
+    Arrays.sort(arr);
+    arr[0].drawTo(arr[arr.length-1]);
+    showResultPoints(arr);
+  }
+
+  public static void checkAdjacentPoints(Point[] ps, Point p0) {
+    if (ps.length < 3) return;
+
+    int c = 0;
+    double prev = p0.slopeTo(ps[0]);
+
+    for (int i = 1; i < ps.length; i++) {
+      double slp = p0.slopeTo(ps[i]);
+
+      if (slp == prev) {
+        c++;
+      } else {
+        if (c >= 3) {
+          outPut(ps, i - 1 - c, i - 1, p0);
+        }
+        c = 0;
+      }
+
+      prev = slp;
+    }
+
+  }
+
   public static void main(String[] args) {
 
     // initialize canvas
@@ -26,11 +105,22 @@ public class Fast {
     }
 
     // test begin
-    Point p0 = points[0];
-    Point[] ps = new Point[N-1];
-    for (int i = 0; i < N-1; i++) {
-      ps[i] = points[i+1];
+
+    for (int i = 0; i < N; i++) {
+      Point p0 = points[i];
+      Point[] ps = rightPoints(i, points);
+      // showPoints(ps);
+      Arrays.sort(ps, p0.SLOPE_ORDER);
+      // showPoints(ps);
+      checkAdjacentPoints(ps, p0);
+
     }
+
+    // Point p0 = points[0];
+    // Point[] ps = new Point[N-1];
+    // for (int i = 0; i < N-1; i++) {
+    //   ps[i] = points[i+1];
+    // }
 
     // look points
     // StdOut.println("p0: " + p0.toString());
@@ -38,34 +128,36 @@ public class Fast {
     //   StdOut.println(p.toString());
     // }
 
-    sortPoints(p0, ps);
+    // sortPoints(p0, ps);
+    // assert isSorted(p0, ps);
 
     // look points
-    StdOut.println("p0: " + p0.toString());
-    for (Point p : ps) {
-      StdOut.println(p.toString());
-      StdOut.println(p0.slopeTo(p));
-    }
+    // StdOut.println("p0: " + p0.toString());
+    // for (Point p : ps) {
+    //   StdOut.println(p.toString());
+    //   StdOut.println(p0.slopeTo(p));
+    // }
 
-    int c = 0;
+    // int c = 0;
     // double slp;
-    double prev = p0.slopeTo(ps[0]);
+    // double prev = p0.slopeTo(ps[0]);
 
-    for (int i = 1; i < ps.length; i++) {
-      double slp = p0.slopeTo(ps[i]);
+    // for (int i = 1; i < ps.length; i++) {
+    //   double slp = p0.slopeTo(ps[i]);
 
-      if (slp == prev) {
-        c++;
-        if (c >= 3) {
-          StdOut.println(ps[i].toString());
-          p0.drawTo(ps[i]);
-        }
-      } else {
-        c = 0;
-      }
+    //   if (slp == prev) {
+    //     c++;
+    //     if (c >= 3) {
+    //       StdOut.println("c:" + c);
+    //       StdOut.println(ps[i].toString());
+    //       p0.drawTo(ps[i]);
+    //     }
+    //   } else {
+    //     c = 0;
+    //   }
 
-      prev = slp;
-    }
+    //   prev = slp;
+    // }
 
     // test end
 
