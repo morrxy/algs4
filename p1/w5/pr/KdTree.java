5274ba934a762708097e9a36daf137289c12cccc
morrxy <morrxy@gmail.com>
2013-10-08 23:49:11 +0800

    m

--- p1/w5/pr/KdTree.java
+++ p1/w5/pr/KdTree.java
@@ -1,5 +1,7 @@
 public class KdTree {
 
+  private Node root;
+
   private static class Node {
     // the point
     private Point2D p;
@@ -9,29 +11,68 @@ public class KdTree {
     private Node lb;
     // the right/top subtree
     private Node rt;
+
+    private int count;
+
+    public Node(Point2D pt, int c) {
+      this.p = pt;
+      this.count = c;
+    }
+
+    public Node() {}
   }
 
   // construct an empty set of points
-  public KdTree() {}
+  public KdTree() {
+    root = new Node();
+  }
 
   // is the set empty?
   public boolean isEmpty() {
-    return false;
+    return size() == 0;
   }
 
   // number of points in the set
   public int size() {
-    return 0;
+    return size(root);
+  }
+
+  private int size(Node x) {
+    if (x == null) return 0;
+    else return x.count;
   }
 
   // add the point p to the set (if it is not already in the set)
   public void insert(Point2D p) {
+    if (contains(p)) return;
+    root = put(root, p);
+  }
 
+  private Node put(Node x, Point2D p) {
+    if (x == null) return new Node(p, 1);
+    int cmp = p.compareTo(x.p);
+    if      (cmp < 0) x.lb = put(x.lb,  p);
+    else if (cmp > 0) x.rt = put(x.rt, p);
+    else              x.p  = p;
+    x.count = 1 + size(x.lb) + size(x.rt);
+    return x;
   }
 
   // does the set contain the point p?
   public boolean contains(Point2D p) {
-    return false;
+    return get(p) != null;
+  }
+
+  private Point2D get(Point2D p) {
+    return get(root, p);
+  }
+
+  private Point2D get(Node x, Point2D p) {
+    if (x == null) return null;
+    int cmp = p.compareTo(x.p);
+    if (cmp < 0) return get(x.lb, p);
+    else if (cmp > 0) return get(x.rt, p);
+    else return x.p;
   }
 
   // draw all of the points to standard draw
@@ -46,5 +87,10 @@ public class KdTree {
   // a nearest neighbor in the set to p; null if set is empty
   public Point2D nearest(Point2D p) {
     return new Point2D(0, 0);
+  }
+
+  public static void main(String[] args) {
+    KdTree tree = new KdTree();
+    StdOut.println(tree.isEmpty());
   }
 }
